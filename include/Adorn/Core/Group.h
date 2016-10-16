a7e2ab6e355ab7914308c45f7cbaf22cac67a48a
adityanichkawade <aditya.nichkawade@gmail.com>
2016-10-16 22:04:08 +0530

    Removing pre-generate license
    Intermediate commit

--- include/Adorn/Core/Group.h
+++ include/Adorn/Core/Group.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Group.h
- * Author: Aditya
- *
- * Created on September 23, 2016, 10:16 PM
- */
-
 #ifndef GROUP_H
 #define GROUP_H
 
